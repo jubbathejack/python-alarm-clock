de1ae2ffc0781c960ad62690d6b0a70a903667b4
Jack Parker <jack@jubba.org>
2019-05-05 18:00:29 +0100

    Initial working timer!

--- timer.py
+++ timer.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+
+import datetime
+
+print("Please enter a time in 24H format like HH:MM:SS.")
+userTime = input()
+
+while userTime != datetime.datetime.now().strftime("%H:%M:%S"):
+    pass
+else:
+    print("Alarm!")
+    exit(0)
