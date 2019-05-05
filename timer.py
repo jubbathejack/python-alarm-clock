3ccac932aa5f81afd092be00ae1b4fda89728835
Jack Parker <jack@jubba.org>
2019-05-05 19:39:53 +0100

    Refine pygame imports.

--- timer.py
+++ timer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import datetime
-import pygame
+from pygame import mixer, mixer_music
 
 print("Please enter a time in 24H format like HH:MM:SS.")
 userTime = input()
@@ -10,9 +10,9 @@ while userTime != datetime.datetime.now().strftime("%H:%M:%S"):
     pass
 else:
     print("Alarm!")
-    pygame.mixer.init()
-    pygame.mixer_music.load("alarm.wav")
-    pygame.mixer_music.play()
-    while pygame.mixer.music.get_busy():
+    mixer.init()
+    mixer_music.load("alarm.wav")
+    mixer_music.play()
+    while mixer.music.get_busy():
         continue
     exit(0)
